0ebd62fc3f365d74406b9ac481bae0b6d33ca87d
HenriIlves <henri@ilves.xyz>
2026-04-25 16:51:21 +0300

    Kirkoli: added state machine and lcd for master/main

--- master/main.c
+++ master/main.c
@@ -15,14 +15,79 @@
 
 #define SLAVE_ADDRESS 0b1010111 // 87 as decimal
 
-int main(void)
-{
-    spi_master_init();
+//Switch case cases
+typedef enum {
+	IDLE,
+	GOING_UP,
+	GOING_DOWN,
+	DOOR_OPENING,
+	OBSTACLE,
+	DOOR_CLOSING,
+	FAULT
+} State;
 
-    while (1)
-    {
-        continue;
-    }
+int main(void) {
+	spi_master_init();
 
-    return (0);
+	// Intitialize LCD
+	lcd_init(LCD_DISP_ON);
+	lcd_clrscr();
+	
+	while (1) {
+		switch(state) {
+
+			//IDLE
+			case IDLE:
+                lcd_clear();
+                lcd_puts("Choose floor:");
+                
+                
+                //Check for fault
+                if(target_floor == current_floor) {
+                    state = FAULT;
+                }
+                break;
+
+			//GOING UP
+			case GOING_UP:
+                lcd_clear();
+                lcd_puts("Current floor: %02d", current_floor);
+                break;
+
+			//GOING DOWN
+			case GOING_DOWN:
+                lcd_clear();
+                lcd_puts("Current floor: %02d", current_floor);
+                break;
+
+			//DOOR OPENING
+			case DOOR_OPENING:
+                lcd_clear();
+                lcd_puts("Door Open");
+                break;
+
+			//OBSTACLE
+			case OBSTACLE:
+                lcd_clear();
+                lcd_puts("Obstacle!");
+                break;
+			
+			//DOOR CLOSING
+			case DOOR_CLOSING:
+                lcd_clear();
+                lcd_puts("Door Closing");
+                break;
+
+			//FAULT
+			case FAULT:
+                lcd_clear();
+                lcd_puts("Same Floor!");
+                
+                _delay_ms(2000);
+
+                state = IDLE;
+                break;
+        }
+	}
+	return (0);
 }
--- master/spi_master.h
+++ master/spi_master.h
@@ -9,7 +9,7 @@ typedef enum state
     GOING_UP,
     GOING_DOWN,
     DOOR_OPENING,
-    OBSTACE_DETECTION,
+    OBSTACLE_DETECTION,
     DOOR_CLOSING,
     FAULT,
 } state_t;
